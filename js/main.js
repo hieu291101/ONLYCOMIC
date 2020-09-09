e70acac132cea37a5827d20dfb3275e02b760fa4
LaHieu <latrunghieuvt1@gmail.com>
2020-09-09 19:13:19 +0700

    9/9

--- js/main.js
+++ js/main.js
@@ -1,4 +1,40 @@
-window.addEventListener('scroll',  function(){
+// carousel
+$(document).ready(function () {
+    $('.autoplay').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 740,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+})
+
+// nav-bar sticky
+window.addEventListener('scroll', function () {
     var nav = document.querySelector('nav');
     nav.classList.toggle('sticky', window.scrollY > 0);
 });
