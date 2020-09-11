5bdf83945e0dd0813a590a88627a082de531c220
LaHieu <latrunghieuvt1@gmail.com>
2020-09-11 22:31:50 +0700

    new

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-// carousel
 $(document).ready(function () {
+    // carousel
     $('.autoplay').slick({
         slidesToShow: 6,
-        slidesToScroll: 1,
+        slidesToScroll: 3,
         autoplay: true,
         autoplaySpeed: 2000,
         dots: true,
@@ -10,7 +10,7 @@ $(document).ready(function () {
                 breakpoint: 1024,
                 settings: {
                     slidesToShow: 4,
-                    slidesToScroll: 3,
+                    slidesToScroll: 2,
                     infinite: true,
                     dots: true
                 }
@@ -19,7 +19,7 @@ $(document).ready(function () {
                 breakpoint: 740,
                 settings: {
                     slidesToShow: 2,
-                    slidesToScroll: 2
+                    slidesToScroll: 1
                 }
             },
             {
@@ -31,6 +31,64 @@ $(document).ready(function () {
             }
         ]
     });
+    // close carousel
+
+    // pagination
+    $('.pagination-item__link-next').click(function () {
+        $('.pagination').find('.pagination-item__page-number.active').next().addClass('active');
+        $('.pagination').find('.pagination-item__page-number.active').prev().removeClass('active');
+    })
+
+    $('.pagination-item__link-prev').click(function () {
+        $('.pagination').find('.pagination-item__page-number.active').prev().addClass('active');
+        $('.pagination').find('.pagination-item__page-number.active').next().removeClass('active');
+    })
+    // close pagination
+
+    //clock
+    // analog
+    const mn_a = document.querySelector('#clock-analog__minute');
+    const hr_a = document.querySelector('#clock-analog__hour');
+    const sc_a = document.querySelector('#clock-analog__second');
+
+    // digital
+    let mn_d = document.querySelector('#clock-digital__minute');
+    let hr_d = document.querySelector('#clock-digital__hour');
+    let sc_d = document.querySelector('#clock-digital__second');
+    let amPm = document.querySelector('#clock-digital_amPM')
+    setInterval(() => {
+        // analog
+        let h_a = new Date().getHours() * 30;
+        let m_a = new Date().getMinutes() * 6;
+        let s_a = new Date().getSeconds() * 6;
+
+        hr_a.style.transform = `rotateZ(${h_a + (m_a / 12)}deg)`;
+        mn_a.style.transform = `rotateZ(${m_a}deg)`;
+        sc_a.style.transform = `rotateZ(${s_a}deg)`;
+
+        // digital
+        let h_d = new Date().getHours();
+        let m_d = new Date().getMinutes();
+        let s_d = new Date().getSeconds();
+        let am = "AM";
+
+        if (h_d > 12) {
+            h_d = h_d - 12;
+            let am = "PM";
+        }
+
+        h_d = (h_d < 10) ? "0" + h_d : h_d;
+        m_d = (m_d < 10) ? "0" + m_d : m_d;
+        s_d = (s_d < 10) ? "0" + s_d : s_d;
+
+        hr_d.innerHTML = h_d + ":";
+        mn_d.innerHTML = m_d + ":";
+        sc_d.innerHTML = s_d + "&nbsp";
+        amPm.innerHTML = am;
+    })
+    // close clock
+
+
 })
 
 // nav-bar sticky
